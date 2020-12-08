2e034a6c69dc899dbd903d0b2a153850db8fdc17
joannafawl <joanna.fawl@madetech.com>
2020-12-08 12:13:27 +0000

    Complete 4 - Add Services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -10,7 +10,7 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero; // name: type
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  // This is a typical "service-in-service" scenario: you inject the MessageService into the HeroService which is injected into the HeroesComponent.
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // send message AFTER fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
